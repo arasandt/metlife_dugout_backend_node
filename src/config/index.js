a54c75759f33fca8bde046ec2bc97769835b1565
Arasan Dayalan <arasan_dt@yahoo.co.in>
2020-12-04 08:16:49 +0000

    changed config to work on heroku platform

--- src/config/index.js
+++ src/config/index.js
@@ -1,37 +1,36 @@
 const { merge } = require('lodash')
-const env = process.env.NODE_ENV || 'development'
+const env = process.env.NODE_ENV
+const platform = process.env.PLATFORM || 'localhost'
 
 const baseConfig = {
   env,
-  isDev: env === 'development',
-  isTest: env === 'testing',
-  port: process.env.PORT || 3000,
+  port: process.env.PORT, 
   secrets: {
     jwt: process.env.JWT_SECRET,
-    jwtExp: process.env.JWT_EXPIRY || '1d'
+    jwtExp: process.env.JWT_EXPIRY
   },
-  dbUrl: process.env.DB_URL || "testdburl"
+  dbUrl: process.env.DB_URL
 }
 
 let envConfig = {}
 
-switch (env) {
-  case 'heroku':
-    envConfig = baseConfig
-    break
-  case 'dev':
-  case 'development':
-    envConfig = require('./dev.env')
-    break
-  case 'test':
-  case 'testing':
-    envConfig = require('./testing.env')
-    break
-  default:
-    envConfig = require('./dev.env')
+if (platform == 'localhost') {
+  switch (env) {
+    case 'test':
+    case 'testing':
+      envConfig = require('./testing.env')
+      break
+    case 'prod':
+    case 'production':
+      envConfig = require('./prod.env')
+      break
+    default:
+      envConfig = require('./dev.env')
+  } 
+} else {
+  envConfig = baseConfig
 }
 
 const config = merge(baseConfig, envConfig) 
-
-console.log("Running in environment :", config.env.toUpperCase())
+console.log(`Running ${config.env.toUpperCase()} environment on ${platform.toUpperCase()}: `)
 module.exports = config
